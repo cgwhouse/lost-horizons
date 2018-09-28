0f0b197766b63da720a6eed997d86302e801214a
Cristian Widenhouse <cgwidenhouse@gmail.com>
2018-09-27 22:18:06 -0400

    make corrections to create tables script and do a bit more in the master script

--- createTables.sql
+++ createTables.sql
@@ -3,6 +3,7 @@ CREATE TABLE Submissions
     submission_ID INTEGER PRIMARY KEY,
     name TEXT NOT NULL,
     url TEXT NOT NULL,
+    submitter_ID INTEGER NOT NULL,
     UNIQUE(name),
     UNIQUE(url),
     FOREIGN KEY (submitter_ID) REFERENCES Members(member_ID)
--- lostHorizons.ps1
+++ lostHorizons.ps1
@@ -9,10 +9,18 @@ try {
   Import-Module PSSQLite -ErrorAction Stop
   }
   catch {
-    Write-Host 'Something needs to be installed, just a second'
+    Write-Host 'Something needs to be installed, just a second...'
     Install-Module PSSQLite -Force
     Write-Host '...SQLite module was installed!'
     Import-Module PSSQLite
   }
 
-Write-Host 'Everything is fine'
+$db = "$PSScriptRoot/data.SQLite"
+if(-not (Test-Path $db)) {
+  Write-Host 'no database detected, we will make one'
+  $query = [IO.File]::ReadAllText("./createTables.sql")
+  Write-Host $query
+  Invoke-SQLiteQuery -DataSource $db -Query $query
+} else {
+  Write-Host 'database detected'
+}
