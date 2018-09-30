ba71cfb5c94b11ad8390c5185ab3b2075aba7160
Cristian Widenhouse <cgwidenhouse@gmail.com>
2018-09-30 14:27:19 -0400

    VSCode ps formatting and slight edits

--- lostHorizons.ps1
+++ lostHorizons.ps1
@@ -2,25 +2,26 @@
 # Cristian Widenhouse
 #
 
-$welcomeBanner = [IO.File]::ReadAllText("./banner.txt")
+$welcomeBanner = [IO.File]::ReadAllText("$PSScriptRoot\banner.txt")
 Write-Host "`n`n$welcomeBanner`n`n`n"
 
 try {
-  Import-Module PSSQLite -ErrorAction Stop
-  }
-  catch {
+    Import-Module PSSQLite -ErrorAction Stop
+}
+catch [Exception] {
     Write-Host 'Something needs to be installed, just a second...'
     Install-Module PSSQLite -Force
     Write-Host '...SQLite module was installed!'
     Import-Module PSSQLite
-  }
+}
 
-$db = "$PSScriptRoot/data.SQLite"
-if(-not (Test-Path $db)) {
-  Write-Host 'no database detected, we will make one'
-  $query = [IO.File]::ReadAllText("./createTables.sql")
-  Write-Host $query
-  Invoke-SQLiteQuery -DataSource $db -Query $query
-} else {
-  Write-Host 'database detected'
+$ds = "$PSScriptRoot/data.SQLite"
+if (-not (Test-Path $ds)) {
+    Write-Host 'no database detected, we will make one'
+    $query = [IO.File]::ReadAllText("$PSScriptRoot\createTables.sql")
+    Write-Host $query
+    Invoke-SQLiteQuery -DataSource $ds -Query $query
+}
+else {
+    Write-Host 'database detected'
 }
