119ad4dcfe573c3dc54b64a19b43d5169760fb95
Cristian Widenhouse <cgwidenhouse@gmail.com>
2018-09-30 21:17:32 -0400

    Create ability to add and remove team members

--- lostHorizons.ps1
+++ lostHorizons.ps1
@@ -3,7 +3,9 @@
 #
 
 $welcomeBanner = [IO.File]::ReadAllText("$PSScriptRoot\banner.txt")
-$mainPrompt = "Available Commands:`n-------------------`n1 - View Songs`n2 - View Team Members"
+$mainPrompt = "`nMain Menu`n`nAvailable Commands:`n-------------------`n1 - View Songs`n2 - View Team Members`n"
+$membersPrompt = "`n`nAvailable Commands:`n-------------------`n1 - Add a Member`n2 - Remove a Member`n"
+$removeMemberQuery = "DELETE FROM Members WHERE name = @name;DELETE FROM Submissions WHERE submitter_ID = (SELECT member_ID FROM Members WHERE name = @name);"
 $dsPath = "$PSScriptRoot/data.SQLite"
 $commands = 1, 2
 
@@ -33,23 +35,54 @@ function ViewSongs {
     Write-Host "this is the view songs command"
 }
 
+function RemoveMember {
+    $name = Read-Host 'Enter name of member to remove, all submissions will be deleted (press enter to cancel)'
+    if (-not ($name)) { return }
+    Invoke-SqliteQuery -DataSource $dsPath -Query $removeMemberQuery -SqlParameters @{name = $name}
+    return
+}
+
+function AddMember {
+    $name = Read-Host 'Enter name of member to add (press enter to cancel)'
+    if (-not ($name)) { return }
+    Invoke-SqliteQuery -DataSource $dsPath -Query "INSERT INTO Members (name) VALUES (@name);" -SqlParameters @{name = $name}
+    return
+}
+
 function ViewMembers {
-    #TODO
-    Write-Host "this is the view members command"
+    while ($true) {
+        $memberList = Invoke-SqliteQuery -DataSource $dsPath -Query "SELECT name AS 'Member List' FROM Members;" | Out-String
+        $memberList.TrimEnd()    
+        Write-Host $membersPrompt
+        $cmd = Read-Host 'Select a command (press enter to return to main menu)'
+        if (-not $cmd) {
+            Write-Host "`nReturning to main menu..."
+            return 
+        }
+        try {
+            $cmd = [int]$cmd
+            if (-not ($commands.Contains($cmd))) { throw }
+        }
+        catch { Write-Host "`nInvalid command, use the numbers on the left" }
+        switch ($cmd) {
+            1 { AddMember }
+            2 { RemoveMember }
+        }
+    }
 }
 
 function Main {
-    Write-Host "`n`n$welcomeBanner`n`n`n" -ForegroundColor Cyan
+    Write-Host "`n`n$welcomeBanner`n`n" -ForegroundColor Cyan
     HandleSqlModule
     while ($true) {
         Write-Host $mainPrompt
-        $cmd = Read-Host 'Select a command (press enter to exit)'
+        $cmd = Read-Host 'Select a command (press enter to exit program)'
         if (-not $cmd) { exit }
         try {
             $cmd = [int]$cmd
             if (-not ($commands.Contains($cmd))) { throw }
         }
-        catch { Write-Host "`nInvalid command, use the numbers on the left`n" }
+        catch { Write-Host "`nInvalid command, use the numbers on the left" }
         switch ($cmd) {
             1 { ViewSongs }
             2 { ViewMembers }
