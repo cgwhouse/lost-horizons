8751657f0f0320bdd072978a2ffceb5e2d850285
Cristian Widenhouse <cgwidenhouse@gmail.com>
2018-09-30 22:38:53 -0400

    Implement adding song and add stuff to db

--- lostHorizons.ps1
+++ lostHorizons.ps1
@@ -7,7 +7,9 @@
 $welcomeBanner = [IO.File]::ReadAllText("$PSScriptRoot\banner.txt")
 $mainPrompt = "`nMain Menu`n`nAvailable Commands:`n-------------------`n1 - View Songs`n2 - View Team Members`n"
 $membersPrompt = "`n`nAvailable Commands:`n-------------------`n1 - Add a Member`n2 - Remove a Member`n"
+$submissionsPrompt = "`n`nAvailable Commands:`n-------------------`n1 - Play a Song`n2 - Add a Song`n"
 $removeMemberQuery = "DELETE FROM Members WHERE name = @name;DELETE FROM Submissions WHERE submitter_ID = (SELECT member_ID FROM Members WHERE name = @name);"
+$viewSubmissionsQuery = "SELECT submission_ID AS 'Submission #', Submissions.name AS 'Submission Name', url AS 'URL', members.name AS 'Submitted By' FROM Submissions INNER JOIN Members ON Submissions.submitter_ID = Members.member_ID;"
 $dsPath = "$PSScriptRoot/data.SQLite"
 $commands = 1, 2
 
@@ -15,9 +17,47 @@ $commands = 1, 2
 
 #region Commands
 
-function ViewSongs {
+function PlaySubmission {
+    $submissionNumber = Read-Host 'Pick the song to play by entering a submission #'
+    try {
+        $submissionNumber = [int]$submissionNumber
+        $query = "SELECT url FROM Submissions WHERE submission_ID = @n;"
+        $url = Invoke-SqliteQuery -DataSource $dsPath -Query $query -SqlParameters @{ n = $submissionNumber } | Out-DataTable
+        $url = $url.Rows[0].url
+        if (-not ($url)) { throw }
+        else {
+            Write-Host "Launching Chrome...`n"
+            Start-Process -FilePath 'C:\Program Files (x86)\Google\Chrome\Application\chrome.exe' -ArgumentList $url
+        }
+    }
+    catch { Write-Host "`nNo Submissions with that Submission # are in the table" }
+}
+
+function AddSubmission {
     #TODO
-    Write-Host "this is the view songs command"
+    Write-Host "adding a song"
+}
+
+function ViewSubmissions {
+    while ($true) {
+        $submissionList = Invoke-SqliteQuery -DataSource $dsPath -Query $viewSubmissionsQuery | Out-String
+        $submissionList.TrimEnd()
+        Write-Host $submissionsPrompt
+        $cmd = Read-Host 'Select a command (press enter to return to main menu)'
+        if (-not $cmd) {
+            Write-Host "`nReturning to main menu..."
+            return
+        }
+        try {
+            $cmd = [int]$cmd
+            if (-not ($commands.Contains($cmd))) { throw }
+        }
+        catch { Write-Host "`nInvalid command, use the numbers on the left" }
+        switch ($cmd) {
+            1 { PlaySubmission }
+            2 { AddSubmission }
+        }
+    }
 }
 
 function RemoveMember {
@@ -68,7 +108,6 @@ function HandleSqlModule {
     }
 }
 
-
 function Main {
     Write-Host "`n`n$welcomeBanner`n`n" -ForegroundColor Cyan
     HandleSqlModule
@@ -82,7 +121,9 @@ function Main {
         }
         catch { Write-Host "`nInvalid command, use the numbers on the left" }
         switch ($cmd) {
-            1 { ViewSongs }
+            1 {
+                ViewSubmissions
+            }
             2 { ViewMembers }
         }
     }
