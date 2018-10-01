077b108e058b4ec1ab1f942a7b887e9ba68e5ee7
Cristian Widenhouse <cgwidenhouse@gmail.com>
2018-09-30 21:22:27 -0400

    add code regions and do some db operations

--- lostHorizons.ps1
+++ lostHorizons.ps1
@@ -2,6 +2,8 @@
 # Cristian Widenhouse
 #
 
+#region Globals
+
 $welcomeBanner = [IO.File]::ReadAllText("$PSScriptRoot\banner.txt")
 $mainPrompt = "`nMain Menu`n`nAvailable Commands:`n-------------------`n1 - View Songs`n2 - View Team Members`n"
 $membersPrompt = "`n`nAvailable Commands:`n-------------------`n1 - Add a Member`n2 - Remove a Member`n"
@@ -9,26 +11,9 @@ $removeMemberQuery = "DELETE FROM Members WHERE name = @name;DELETE FROM Submiss
 $dsPath = "$PSScriptRoot/data.SQLite"
 $commands = 1, 2
 
-# Make the database if it doesn't exist for some reason, this may go in a recovery script or not exist
-# if (-not (Test-Path $dsPath)) {
-#     Write-Host 'No database detected, creating one and adding members'
-#     $createTablesQuery = [IO.File]::ReadAllText("$PSScriptRoot\createTables.sql")
-#     Invoke-SQLiteQuery -DataSource $dsPath -Query $createTablesQuery
-#     $insertMemberQuery = "INSERT INTO Members (name) VALUES (@name);"
-#     foreach ($line in (Get-Content "$PSScriptRoot\members.txt")) {
-#         Invoke-SQLiteQuery -DataSource $dsPath -Query $insertMemberQuery -SqlParameters @{name = $line}
-#     }
-# }
+#endregion Globals
 
-function HandleSqlModule {
-    try { Import-Module PSSQLite -ErrorAction Stop }
-    catch {
-        Write-Host 'Something needs to be installed, just a second...'
-        Install-Module PSSQLite -Force -Scope CurrentUser
-        Write-Host "...SQLite module was installed!`n"
-        Import-Module PSSQLite
-    }
-}
+#region Commands
 
 function ViewSongs {
     #TODO
@@ -70,6 +55,19 @@ function ViewMembers {
         }
     }
 }
+
+#endregion Commands
+
+function HandleSqlModule {
+    try { Import-Module PSSQLite -ErrorAction Stop }
+    catch {
+        Write-Host 'Something needs to be installed, just a second...'
+        Install-Module PSSQLite -Force -Scope CurrentUser
+        Write-Host "...SQLite module was installed!`n"
+        Import-Module PSSQLite
+    }
+}
+
 
 function Main {
     Write-Host "`n`n$welcomeBanner`n`n" -ForegroundColor Cyan
